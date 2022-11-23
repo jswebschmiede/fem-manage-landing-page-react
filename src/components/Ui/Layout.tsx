47f552ab833ad6affa91fe8585b0bf78aff62032
jswebschmiede <joergschoeneburg@freenet.de>
2022-11-24 00:24:57 +0100

    ???

--- src/components/Ui/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import { LayoutProps } from "../../typings";
-
-const Layout = ({ children }: LayoutProps) => {
-  return <div className="wrapper relative overflow-hidden">{children}</div>;
-};
-
-export default Layout;
